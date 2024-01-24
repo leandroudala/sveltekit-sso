4c19d0862140d0d2a4b9428d93fcd4aed6d8b9ee
Leandro Udala <leandro.correia@radixeng.com.br>
2024-01-24 01:03:48 +0000

    auto redirect to correct urls when user is or is not logged in

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,18 @@
 import { getLoggedUser } from "$lib/utils/user.utils";
 import { redirect } from "@sveltejs/kit";
 
+const ALLOWED_ANONYMOUS_PATH = new Set(['/', '/login', '/register']);
+
 export async function load({cookies, url}) {
     const tokenData = getLoggedUser(cookies);
 
-    if (!url.pathname.startsWith('/home') && !url.pathname.startsWith('/logout') && tokenData) {
+    if (ALLOWED_ANONYMOUS_PATH.has(url.pathname) && tokenData) {
+        // redirecting to home page if user is logged in
         throw redirect(303, '/home');
+    }
+
+    if (!ALLOWED_ANONYMOUS_PATH.has(url.pathname) && !tokenData) {
+        // redirecting to login page if user is not logged in 
+        throw redirect(303, '/');
     }
 }
